3d005bc8cf52508579d4005b10ea80ac96c9c853
TautCoder <singhutkarsh187@gmail.com>
2026-06-07 17:57:32 +0530

    feat: add Matching screen with score tiers, AI insights overlay, and animated score bars

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
-import CustomerDetailPage from './pages/CustomerDetailPage'
+import MatchingPage from './pages/MatchingPage'
 
 export default function App() {
   // TODO: React Router will wire all pages together.
-  return <CustomerDetailPage />
+  return <MatchingPage />
 }
+
 
